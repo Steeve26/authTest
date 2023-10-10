e684a489ff4e396fba849853d9adba9d0ae62a06
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-10-10 22:41:35 +0300

    test commit

--- src/services/auth0.services.js
+++ src/services/auth0.services.js
@@ -5,7 +5,7 @@ const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
 const userScope = import.meta.env.VITE_REACT_APP_AUTH0_USER_SCOPE;
 
 export const auth = new auth0.WebAuth({
-  domain: domain,
+  domain: "dev-8jubc8723wwh03dy.us.auth0.com",
   clientID: clientId,
   scope: userScope
 });
