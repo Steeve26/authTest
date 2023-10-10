add778f737336ec6fb1d8bd800932ab7fd29e73d
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-10-10 22:44:16 +0300

    test commit

--- src/services/auth0.services.js
+++ src/services/auth0.services.js
@@ -6,8 +6,8 @@ const userScope = import.meta.env.VITE_REACT_APP_AUTH0_USER_SCOPE;
 
 export const auth = new auth0.WebAuth({
   domain: "dev-8jubc8723wwh03dy.us.auth0.com",
-  clientID: clientId,
-  scope: userScope
+  clientID: "ixGqSzFIrDkuA5twSGcfrAQo4opIiyai",
+  scope: "openid email profile"
 });
 
 
