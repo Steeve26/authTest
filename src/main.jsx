6c880e41982a97d891bd1fbdb5dea32ac560ee1c
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-10-10 21:38:58 +0300

    removed unecessary import

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter as Router } from 'react-router-dom';
-
-const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}>
         <App />
-      </Auth0Provider>
     </Router>  
   </React.StrictMode>,
 )
