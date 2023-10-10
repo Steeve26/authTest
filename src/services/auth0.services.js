e60577de9d4d645731d8b358edf60639695b2fc4
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-10-10 22:31:41 +0300

    service bug fix

--- src/services/auth0.services.js
+++ src/services/auth0.services.js
@@ -5,7 +5,7 @@ const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
 const userScope = import.meta.env.VITE_REACT_APP_AUTH0_USER_SCOPE;
 
 export const auth = new auth0.WebAuth({
-  domain,
+  domain: domain,
   clientID: clientId,
   scope: userScope
 });
